f7a35e928e8fd3f2c26420c47083908ce2927fca
IzukiKa <isaacjroberts0505@gmail.com>
2023-03-08 23:33:31 +0000

    Update main.py

--- main.py
+++ main.py
@@ -7,21 +7,10 @@ from nbt import nbt
 from discord.ext import commands
 from discord import app_commands
 from dotenv import load_dotenv
+from libs import nyabot
 
-class NyaBot(commands.Bot):
-	def __init__(self, command_prefix: str = "!"):
-		super().__init__(command_prefix=command_prefix, case_insensitive=True, intents=discord.Intents.all())
 
-	@staticmethod
-	def is_owner():
-		def owner(interaction: discord.Interaction):
-			if interaction.user.id == 483721725205282858:
-				return True
-			else:
-				return app_commands.MissingPermissions("Only the bot owner is allowed to use this command!")
-		return app_commands.check(owner)
-
-client = NyaBot()
+client = nyabot.NyaBot()
 
 logger = logging.getLogger("discord")
 logger.setLevel(logging.INFO)
@@ -39,6 +28,11 @@ logger.addHandler(handler)
 
 discord.utils.setup_logging(handler=handler, formatter=formatter, level=logging.INFO, root=True)
 
+cog_list = sorted([
+	f"{file[:-3]}"
+	for file in os.listdir("./cogs/")
+	if file.endswith(".py")
+])
 
 async def run():
 	def get_env() -> dict:
@@ -51,39 +45,34 @@ async def run():
 	environment = get_env()
 	await client.start(environment["TOKEN"])
 
+async def file_init():
+	if not os.path.exists("./logs/"):
+		os.mkdirs("./logs/")
+		
+	if not os.path.exists("./nuker/export/"):
+		logging.info("Creating nuker directory..")
+		os.makedirs("./nuker/export/")
+
 @client.event
 async def setup_hook():
+	await file_init()
+	
+	for filename in os.listdir("./cogs/"):
+		if filename.endswith(".py"):
+			await client.load_extension(f"cogs.{filename[:-3]}")
+
 	print("NyaBot is now online!")
 
 @client.command(name="sync")
-@NyaBot.is_owner()
+@nyabot.is_owner()
 async def sync(ctx: commands.context.Context):
 	synced = await client.tree.sync()
 	await ctx.send(f"Synced {len(synced)} commands")
+	
 
-@client.command(name="biomenuke")
-@app_commands.describe(
-	dimension="The dimension that the biomes are in",
-	namespace="The namespace of the biomes. Usually the mod's name.",
-	data_file="Your level.dat file."
-)
-async def biomenuke(interaction: discord.Interaction, dimension: str, namespace: str, data_file: discord.Attachment):
-	logging.info(f"Nuking biomes for user {interaction.message.author}")
-	if not os.path.exists("./nuker/export/"):
-		logger.info("Creating nuker directory..")
-		os.makedirs("./nuker/export/")
-	await data_file.save(fp="./nuker/export/temp.dat")
-
-	nbtfile = nbt.NBTFile("./nuker/export/temp.dat", "rb")
-	biomes_location = nbtfile["Data"]["WorldGenSettings"]["dimensions"][dimension]["generator"]["biome_source"]["biomes"]
-	for index, tag in enumerate(biomes_location):
-		biome = str(tag['biome']).split(":")
-		if biome[0] == namespace:
-			logger.debug(f"Nuking biome: {biome}")
-			biomes_location[index].clear()
-	nbtfile.write_file("./nuker/export/level.dat")
-	logger.info("Biomes successfully nuked!")
-	await interaction.message.reply("Biomes successfully nuked!", file=discord.File("./nuker/export/level.dat"))
+@client.event
+async def on_command_error(ctx, error):
+	raise error
 
 if __name__ == "__main__":
 	try:
